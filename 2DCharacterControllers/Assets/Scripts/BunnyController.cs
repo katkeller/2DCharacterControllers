25b9c9e4a16c229d29b9afd407ebc63c5bdbb482
Kathryn Keller <Kathryn.Keller@iam.local>
2018-10-24 14:03:39 -0500

    Added BunnyController script, modified with rb2d

---
+++ 2DCharacterControllers/Assets/Scripts/BunnyController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyController : MonoBehaviour {
+
+    public float MaxSpeed = 10f;
+    bool FacingRight = true;
+    public Rigidbody2D rb2d;
+
+	void Start ()
+    {
+		
+	}
+	
+	void FixedUpdate ()
+    {
+        float move = Input.GetAxis("Horizontal");
+
+        //Rigidbody2D.velocity = new Vector2(move * MaxSpeed, Rigidbody2D.velocity.y);
+
+        rb2d.velocity = new Vector2(move * MaxSpeed, rb2d.velocity.y);
+	}
+}
